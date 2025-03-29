4890030010d3a724260cc647bb269e9073b976ec
harshithharsh <harshith.as@polussolutions.com>
2025-03-29 12:08:44 +0530

    Initial commit

---
+++ src/main/java/com/harshith/contestpro/ContestproApplication.java
@@ -0,0 +1,13 @@
+package com.harshith.contestpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshith/contestpro/ContestproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshith.contestpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
